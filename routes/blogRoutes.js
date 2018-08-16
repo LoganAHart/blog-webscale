e10e402e77d9bc1b4146d261acd019e9b09d712a
Logan Hart <2loganhart@gmail.com>
2018-08-16 10:34:17 -0700

    replace redis get/set with hget/hset, now nesting blogs via user ID, add clearhash helper functions for use when a user posts a new blog

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,6 +1,7 @@
 
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const { clearHash } = require('../services/cache');
 
 const Blog = mongoose.model('Blog');
 
@@ -15,7 +16,9 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id }).cache();
+    const blogs = await Blog
+      .find({ _user: req.user.id })
+      .cache({ key: req.user.id });
     res.send(blogs);
   });
 
@@ -32,5 +35,6 @@ module.exports = app => {
     } catch (err) {
       res.send(400, err);
     }
+    clearHash(req.user.id);
   });
 };
--- services/cache.js
+++ services/cache.js
@@ -4,12 +4,13 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function() {
+mongoose.Query.prototype.cache = function(options = {}) {
   this._cache = true;
+  this._key = JSON.stringify(options.key || 'default');
   return this;
 }
 
@@ -20,7 +21,7 @@ mongoose.Query.prototype.exec = async function () {
   const key = JSON.stringify(Object.assign({}, this.getQuery(), {
       collection: this.mongooseCollection.name
   }));
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this._key, key);
   if (cacheValue) {
     console.log('using cache')
     const doc = JSON.parse(cacheValue);
@@ -29,6 +30,12 @@ mongoose.Query.prototype.exec = async function () {
       : new this.model(doc);
   }
   const result = await exec.apply(this, arguments);
-  client.set(key, JSON.stringify(result));
+  client.hset(this._key, key, JSON.stringify(result));
   return result;
 }
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  }
+};
