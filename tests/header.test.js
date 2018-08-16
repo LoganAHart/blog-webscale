6ea8cbaa2388f71e11c76d0bd44bac5c2f839087
Logan Hart <2loganhart@gmail.com>
2018-08-16 16:15:01 -0700

    Implement initial tests for oath flow in app, authenticate a user/mock the session and session.sig values in the user's cookie

--- tests/header.test.js
+++ tests/header.test.js
@@ -18,3 +18,32 @@ test('the header has the correct text', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
   expect(text).toEqual('Blogster');
 });
+
+test('clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+  const id = '5b7364e20af4394ba41d0da2';
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString);
+
+  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('localhost:3000');
+  await page.waitFor('a[href="/auth/logout"]');
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+  expect(text).toEqual('Logout');
+});
