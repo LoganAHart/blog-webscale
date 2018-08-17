25957559714775567d45c5dacc0bd0c4044869ec
Logan Hart <2loganhart@gmail.com>
2018-08-17 12:13:19 -0700

    remove login logic from header.tests.js, refactor helpers/page.js to include login logic, refactor to remove test user from db on page close

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const userFactory = require('../factories/userFactory');
+const sessionFactory = require('../factories/sessionFactory');
 
 class CustomPage {
   static async build() {
@@ -7,17 +9,36 @@ class CustomPage {
     });
 
     const page = await browser.newPage();
-    const customPage = new CustomPage(page);
+    const customPage = new CustomPage(page, browser);
 
     return new Proxy(customPage, {
       get: function(target, property) {
-        return customPage[property] || browser[property] || page[property];
+        return customPage[property] || page[property] || browser[property];
       }
     });
   }
 
-  constructor(page) {
+  constructor(page, browser) {
     this.page = page;
+    this.browser = browser;
+  }
+
+  async login() {
+    this.user = await userFactory.createUser();
+    const { session, sig } = sessionFactory(this.user);
+    const url = this.page.url();
+
+    await this.page.setCookie({ name: 'session', value: session });
+    await this.page.setCookie({ name: 'session.sig', value: sig });
+    await this.page.goto(url);
+    await this.page.waitFor('a[href="/auth/logout"]');
+  }
+
+  async close() {
+    if (this.user) {
+      await userFactory.deleteUser(this.user);
+    }
+    await this.browser.close();
   }
 }
 
